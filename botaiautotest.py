b1b9ba267136c433f96be8dd51c24e3464756f65
wangxun <troy.wang@beyondsoft.com>
2014-10-17 18:18:34 +0800

    3333333

--- botaiautotest.py
+++ botaiautotest.py
@@ -3,12 +3,14 @@
 
 import os
 import unittest
-
+from selenium.webdriver.common.by import By
 from appium import webdriver
 from time import sleep
 from _elementtree import Element
 import HTMLTestRunner
 import datetime
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Returns abs path relative to this file and not cwd
 PATH = lambda p: os.path.abspath(
@@ -23,6 +25,7 @@ class BotaiAndroidTests(unittest.TestCase):
         desired_caps['platformName'] = 'Android'
         desired_caps['platformVersion'] = '4.2。2'
 #        desired_caps['deviceName'] = 'Android Emulator'
+
         desired_caps['deviceName'] = 'ZTEU930HD'        
         desired_caps['app'] = PATH(
             u'D:/share/软件安装包/iVokaMINIX_1.6.0.apk'
@@ -30,6 +33,7 @@ class BotaiAndroidTests(unittest.TestCase):
         desired_caps['appPackage'] = 'com.pateo.mobile'
         desired_caps['appActivity'] = 'com.pateo.mobile.ui.account.LoginActivity'
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.wait = WebDriverWait(self.driver, 10)        
 
 
         
@@ -44,12 +48,14 @@ class BotaiAndroidTests(unittest.TestCase):
         self.findelementbyclassnameindex('android.widget.EditText', 2, 'password').send_keys(self.password)
         self.findelementbyclassnameindex('android.widget.Button', 1, 'login').click()
 
+
     def entersettingpage(self):
         self.login('13659874858', '123456')
-        sleep(5)
-        self.findelementbyclassnameindex('android.widget.ImageView', 4, 'setting').click()
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/mainpage_date_top')))        
+        self.driver.find_element_by_id('com.pateo.mobile:id/mainpage_thr_img').click()
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/iv_icon')))
         self.findelementbyclassnameindex('android.widget.LinearLayout', 16, 'systemsetting').click()
-        sleep(1)
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/lin_setting_user_info')))
         
     def cleartext(self,Element):
         s=Element.get_attribute('text') 
@@ -122,7 +128,7 @@ class BotaiAndroidTests(unittest.TestCase):
     def getsecondtext(self,ele):
         self.element = ele
         self.a = self.element.text
-        return self.a.split(',')[1]
+        return self.a.split('.')[1]
         
       
 
@@ -131,7 +137,14 @@ class login(BotaiAndroidTests):
     def test_login(self):
         u'''登陆成功'''
         self.login('13659874858', '123456')
-        sleep(5)
+#        self.wait.until(EC.presence_of_element_located((By.CLASS_NAME,'android.widget.Toast')))
+
+
+#        print self.driver.find_element_by_class_name('android.widget.Toast').text
+        
+        
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/mainpage_date_top')))
+
         self.assertIn(u'今天', self.gettextviewtext())
         
 class setting(BotaiAndroidTests):       
@@ -146,8 +159,9 @@ class setting(BotaiAndroidTests):
         self.driver.find_element_by_id('com.pateo.mobile:id/btn_setting_user_take_picture').click()
         sleep(1)
         self.driver.tap([(536,1786)], 1000)    
-        sleep(1)
+        sleep(2)
         self.driver.find_element_by_id('com.sec.android.app.camera:id/save').click()  
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/send_button')))
         self.driver.find_element_by_id('com.pateo.mobile:id/send_button').click()
         
     def personalinformation(self,nickname,contact,telephone,drivernumber):
@@ -174,11 +188,11 @@ class setting(BotaiAndroidTests):
                        
     def test_feedback(self):
         u'''发送反馈测试用例'''
-        self.value = '111111'
+        self.value = '22222'
         self.entersettingpage()     
         self.driver.find_element_by_id('com.pateo.mobile:id/lin_setting_feedback').click()      
         self.feedback(self.value)
-        sleep(8)
+        sleep(10)
         self.assertIn(self.value, self.findelementbyclassnameindex('android.widget.TextView', 3, 'feedbackvalue').text)
         
     def test_personalinformation(self):
@@ -189,48 +203,66 @@ class setting(BotaiAndroidTests):
         self.contact = '1'
         self.telephone = None
         self.drivernumber = None
-        self.nickmame1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_nickname')) + self.nickname
-        self.contact1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact')) + self.contact
-        self.telephone1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact_phone')) + self.telephone
-        self.drivernumber1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_driver_num')) + self.drivernumber        
+        now = datetime.datetime.now()
+        delta = datetime.timedelta(days=-1)
+        a = now + delta
+        self.date = a.strftime('%Y-%m-%d')
+
+
+        
+        if self.nickname:
+            self.nickmame1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_nickname')) + self.nickname
+
+        if self.contact:
+            self.contact1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact')) + self.contact
+
+        if self.telephone:
+            self.telephone1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact_phone')) + self.telephone
+        if self.drivernumber:
+            self.drivernumber1 = self.getsecondtext(self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_driver_num')) + self.drivernumber        
         self.personalinformation(self.nickname, self.contact, self.telephone, self.drivernumber)
         
         self.driver.find_element_by_id('com.pateo.mobile:id/tv_setting_user_effective').click()
-        self.findelementbyclassnameindex('android.widget.ImageButton', 2, 'year').click()
-        self.findelementbyclassnameindex('android.widget.ImageButton', 4, 'month').click()
         self.findelementbyclassnameindex('android.widget.ImageButton', 6, 'day').click()        
         self.driver.find_element_by_id('android:id/button1').click()        
         self.driver.find_element_by_id('com.pateo.mobile:id/tv_setting_user_effective_year').click()
         self.driver.find_element_by_id('com.pateo.mobile:id/tv_year_option_3').click()
-        sleep(1)        
+       
         self.driver.find_element_by_id('com.pateo.mobile:id/iv_setting_user_head_pic').click()   
           
         self.driver.find_element_by_id('com.pateo.mobile:id/btn_setting_user_take_picture').click()
-        sleep(1)
+        sleep(4)
+
         self.driver.tap([(536,1786)], 1000)    
-        sleep(2)
-        self.driver.find_element_by_id('com.sec.android.app.camera:id/save').click()   
-        sleep(1)           
         
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.sec.android.app.camera:id/save')))
+        self.driver.find_element_by_id('com.sec.android.app.camera:id/save').click()   
+      
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/activity_frame_title_btn_right')))
         self.driver.find_element_by_id('com.pateo.mobile:id/activity_frame_title_btn_right').click()
-        sleep(2)
+        sleep(10)
+        self.wait.until(EC.element_to_be_clickable((By.ID,'com.pateo.mobile:id/et_setting_user_nickname')))
         self.driver.find_element_by_id('com.pateo.mobile:id/activity_frame_title_btn_left').click()
         self.driver.find_element_by_id('com.pateo.mobile:id/lin_setting_user_info').click()      
+        sleep(1)
         
-        self.assertIn(self.nickmame1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_nickname'))
-        self.assertIn(self.contact1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact'))
-        self.assertIn(self.telephone1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact_phone'))
-        self.assertIn(self.drivernumber1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_driver_num'))         
+
+
+        
+#        self.assertIn(self.nickmame1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_nickname').text)
+#        self.assertIn(self.contact1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact').text)
+#        self.assertIn(self.telephone1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_emergency_contact_phone').text)
+#        self.assertIn(self.drivernumber1, self.driver.find_element_by_id('com.pateo.mobile:id/et_setting_user_driver_num').text)         
         self.assertIn(u'长期', self.driver.find_element_by_id('com.pateo.mobile:id/tv_setting_user_effective_year').text)
-        self.assertIn('2013-9-15', self.driver.find_element_by_id('com.pateo.mobile:id/tv_setting_user_effective').text)        
+        self.assertIn(self.date, self.driver.find_element_by_id('com.pateo.mobile:id/tv_setting_user_effective').text)        
 
 if __name__ == '__main__':
     suite_login = unittest.TestLoader().loadTestsFromTestCase(login)
     suite_setting = unittest.TestLoader().loadTestsFromTestCase(setting)
     alltests =  unittest.TestSuite([suite_login,suite_setting])
-    test = unittest.TestLoader().loadTestsFromName('botaiautotest.setting.test_personalinformation')
-#    unittest.TextTestRunner(verbosity=2).run(alltests)
-   
+    test = unittest.TestLoader().loadTestsFromName('botaiautotest.setting.test_feedback')
+    unittest.TextTestRunner(verbosity=2).run(test)
+'''   
     filename = 'D:/autotestreport/ALE_SERVER_test/testreport_' + datetime.datetime.now().strftime('%Y-%m-%d %H-%M') + '.html' 
     fp=file(filename,'wb')
     runner = HTMLTestRunner.HTMLTestRunner(
@@ -239,3 +271,4 @@ if __name__ == '__main__':
                                description=u'主要测试登陆、设置两个模块'
                                )
     runner.run(alltests)
+'''
