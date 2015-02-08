f2c8b7e52f4adf450439a5dd2abd4a599eac8f9f
troywang <troy.wang@beyondsoft.com>
2015-02-08 20:42:06 +0800

    3434

--- server.py
+++ server.py
@@ -12,24 +12,86 @@ from thread import *
 from SocketServer import TCPServer, BaseRequestHandler  
 import SocketServer
 import threading
-                      
-class TestServer():       
+import MySQLdb
+import string
+
+class TestServer():   
+    
+    uniquecode = "%uniquecode123%,"      
+    createteble = """CREATE TABLE MESSAGE (
+         ID  INT,
+         TOID INT, 
+         QUNID INT, 
+         NICKNAME  CHAR(20),
+         MESSAGE CHAR(40)
+         )"""     
+         
+
     def __init__(self, ip='localhost', iPort=50007):      
           
         self.ip = ip  
         self.iPort = iPort  
         
+    def savemysql(self):
+
+        self.data1 = self.getdata(self.data)
+
+        conn=MySQLdb.connect(host='localhost',user='root',passwd='123456',db='test',port=3306)
+        cur=conn.cursor()
+        if cur.execute('show tables like "message"') == 0:
+            cur.execute(self.createteble)
+#            cur.execute('drop table if exists message')
+        try:
+#            cur.execute(self.createteble)
+
+            cur.execute("INSERT INTO MESSAGE(ID, \
+       TOID, QUNID, NICKNAME, MESSAGE) \
+       VALUES ('%d', '%d', '%d', '%s', '%s' )" % \
+       (string.atoi(self.data1['id']), string.atoi(self.data1['toid']), string.atoi(self.data1['qunid']), self.data1['nickname'], self.data1['message']))
+
+            cur.close()
+            conn.commit()
+            conn.close()
+        except:
+            conn.rollback()
+        
+        
+        
     def run(self,conn,conns):
 
         while True:   
-            data = conn.recv(1024)
-            if not data:
+            self.data = conn.recv(1024)
+            self.savemysql()
+
+            if not self.data:
                 break       
             for i in conns:
 #                if i != conn:
-                    i.send(data)
+                    i.send(self.data)
                     
-                    
+    def getdata(self,data):
+        
+        newdata={}
+        qunid = data[6:data.find(self.uniquecode)]
+        newdata['qunid']= qunid
+        f = data[data.find(self.uniquecode)+16:]
+        id = f[3:f.find(self.uniquecode)]
+        newdata['id'] = id
+        e = f[f.find(self.uniquecode)+16:]
+        toid = e[5:e.find(self.uniquecode)]
+        newdata['toid'] = toid
+        c= e[e.find(self.uniquecode)+16:]
+        nickname=c[9:c.find(self.uniquecode)]
+        newdata['nickname']=nickname
+        a=c[c.find(self.uniquecode)+16:]        
+        sendtime = a[9:a.find(self.uniquecode)]
+        newdata['sendtime']=sendtime
+        b = a[a.find(self.uniquecode)+16:]
+        
+        message = b[8:]
+        newdata['message']=message
+        return newdata
+                            
                     
     def start(self):     
         sockobj = socket(AF_INET, SOCK_STREAM)       
--- test11.py
+++ test11.py
@@ -1,18 +1,69 @@
-Str1 = 'nickname:888%uniquecode123%,sendtime:2015-02-07 13:12:50%uniquecode123%,message:6666'
-Str2 = '%uniquecode123%,'
-nickname=Str1[9:Str1.find(Str2)]
-print nickname
-a=Str1[Str1.find(Str2)+16:]
+import MySQLdb
+import string
+sql = """CREATE TABLE EMPLOYEE1 (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )""" 
+sql1 = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+sql2 = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+       LAST_NAME, AGE, SEX, INCOME) \
+       VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
+       ('Mac', 'Mohan', 20, 'M', 2000)
+       
+       
+sql3 = "INSERT INTO EMPLOYEE(FIRST_NAME,\
+         LAST_NAME, AGE, SEX, INCOME) VALUES ('%s', '%s', '%d', '%c', '%d')"  
+param = (('Mac', 'Mohan', 20, 'M', 20001), ('Mac', 'Mohan', 20, 'M', 20001), ('Mac', 'Mohan', 20, 'M', 20002))  
 
-sendtime = a[9:a.find(Str2)]
-print sendtime
-b = a[a.find(Str2)+16:]
+sql4= "INSERT INTO MESSAGE(ID, \
+       TOID, QUNID, NICKNAME, MESSAGE) \
+       VALUES ('%d', '%d', '%d', '%s', '%s' )" %        (454, 666, 45, '454', '5656')   
 
-message = b[8:]
-print message
 
-data={}
-data['nickname']=nickname
-data['sendtime']=sendtime
-print data
+createteble = """CREATE TABLE MESSAGE (
+         ID  INT,
+         TOID INT, 
+         QUNID INT, 
+         NICKNAME  CHAR(20),
+         MESSAGE CHAR(40)
+         )"""   
+    
+try:
+    conn=MySQLdb.connect(host='localhost',user='root',passwd='123456',db='test',port=3306)
+    cur=conn.cursor()
+    try:
+        if cur.execute('show tables like "message"') == 0:
+            cur.execute(createteble)
+            
+#        cur.execute('drop table if exists message')
+#        cur.execute(sql4)
+        cur.execute('select * from message where id =1 or toid=1')
+#        cur.executemany(sql3, param)
+        data = cur.fetchall()
+        for i in data:
+            if i[2]==0:                
+                if i[1]==1:
+                    print 'receive:'+str(i)
+                if i[0]==1:
+                    
+                    print "send:"+str(i)
+            else:
+                if i[1]==1:
+                    print 'qunreceive:'+str(i)
+                if i[0]==1:
+                    
+                    print "qunsend:"+str(i)
 
+
+        cur.close()
+        conn.commit()
+        conn.close()
+    except:
+        conn.rollback()
+
+except MySQLdb.Error,e:
+     print "Mysql Error %d: %s" % (e.args[0], e.args[1])
